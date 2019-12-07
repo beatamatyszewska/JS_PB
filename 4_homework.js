8cf4a6d4ed18199ad4e83a8aba67bf4d88015ef3
Beata Matyszewska <beata.matysz@gmail.com>
2019-12-07 13:52:28 +0100

    commit 4

--- 4_homework.js
+++ 4_homework.js
@@ -1,25 +1,40 @@
 //////// --> TASK 1 <--
-//Extend String type with the reverse() function. 
+//Extend String type with the reverse() function.
 //The function is to reverse the value of the string on which it was called.
 
 // String.prototype.reverse = function() {
-//     return this.split("").reverse().join(""); // this oznacza obiekt string / split zmienia ciag string na tablice 
+//     return this.split("").reverse().join(""); // this oznacza obiekt string / split zmienia ciag string na tablice
 // };
 // let test = "Ala ma kota";
 // console.log(test.reverse());
 
 //////// --> TASK 2 <--
-//Extend Number type with the reverse() function. 
+//Extend Number type with the reverse() function.
 //The function is to reverse the value of the Number on which it was called.
 
 // Number.prototype.reverse = function() {
-//     return this.toString().split("").reverse().join(""); // this oznacza obiekt string / split zmienia ciag  string na tablice 
+//     return this.toString().split("").reverse().join(""); // this oznacza obiekt string / split zmienia ciag  string na tablice
 // };
 // let test = 1234567;
 // console.log(test.reverse());
 
 //////// --> TASK 3 <--
-// Based on included JSON file. 
+// Based on included JSON file.
+
+// function DetailsOfPayent(f, g, h) {
+//     (this.Type = 3), //f
+//       (this.company = "ECSTASIA"), //g
+//       (this.date = "09-07-2014"); //h
+//   }
+
+//   function MainEntry(a, s, d, f, g, h) {
+//     (this.index = 0), //a
+//       (this._id = "5c01097e566bb68fe9611b25"), //s
+//       (this.cost = 434.25), //d
+//       (this.detailsOfPayent = new DetailsOfPayent(f, g, h));
+//   }
+
+//Bieda próba
 // const jsonFile = require("./data.json");
 // console.log(jsonFile);
 
@@ -55,15 +70,14 @@
 
 // let mapData = dataFromFile.map(item => {
 //     return `
-//     index: ${item.index}, 
-//     _id: ${item._id}, 
+//     index: ${item.index},
+//     _id: ${item._id},
 //     cost: ${item.cost},
 //     detailsOfPayent: ${item.detailsOfPayment}`
 // });
 
 // console.log(mapData);
 
-
 // // --- d) Create object that will give us data about:
 // // --- d.1) How much money was spend in 2014
-// // --- d.2) What company was earning how much 
+// // --- d.2) What company was earning how much
