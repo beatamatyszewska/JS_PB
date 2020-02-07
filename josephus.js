2afc320969c236c9446df96020ee3befbda6606f
Beata Matyszewska <beata.matysz@gmail.com>
2020-02-07 20:56:24 +0100

    commit 6

---
+++ josephus.js
@@ -0,0 +1,20 @@
+//Josephus problem 
+function josephus(soldiersNumber) {
+  let soldiers = [];
+  for (let i = 1; i < (soldiersNumber + 1); i++) {
+    soldiers.push(i)
+  }
+  do {
+    for (let i = 0; i < soldiers.length; i++) {
+      soldierToKill = i + 1;
+      if ((soldierToKill) === soldiers.length) {
+        soldierToKill = 0;
+      }
+      console.log(`Soldier ${soldiers[i]} killed ${soldiers[soldierToKill]}`);
+      soldiers.splice(soldierToKill, 1);
+    }
+  } while (soldiers.length > 1);
+  console.log(`Soldier ${soldiers} survived!`)
+}
+
+josephus(6)
